4abff2cbe6b66a7d0b0b1fe70519820d15813a1a
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-03-12 17:54:34 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const firstTestimonial = document.querySelector('.div-4');
 const secondTestimonial = document.querySelector('.div-5');
 const thirdTestimonial = document.querySelector('.div-6');
 
-// Change the testimonial to see, only on phone
+// Change the testimonial to see (only on phone)
 dotsContainer.addEventListener('click', (e) => {
     const children = dotsContainer.children;
 
@@ -92,9 +92,11 @@ function handleTouchStart(e) {
 
     // if the user clicks on the screen then allow him to change the profile on swipe
     allolwSwipe = true;
+    console.log('Userul are voi sa face swipe din nou: ' + allolwSwipe);
 }
 
 function handleTouchMove(e) {
+    console.log('Se activeaza functia de swipe');
     let touchMove = e.touches[0].clientX;
  
     // moveChange will keep the difference from the starting point of touch and the current touch point on X axis
@@ -119,6 +121,7 @@ function handleTouchMove(e) {
 
             dotsContainer.children[1].classList.remove('selected');
             dotsContainer.children[2].classList.remove('selected');
+            console.log('Swipe left');
         }
         else if (currentProfileDetails == 2) {
             // display the correct info based on their number
@@ -133,8 +136,10 @@ function handleTouchMove(e) {
             // "unselect" other two orange dots
             dotsContainer.children[0].classList.remove('selected');
             dotsContainer.children[2].classList.remove('selected');
+            console.log('Swipe left');
         }
         else if (currentProfileDetails == 3) {
+        console.log('Swipe right');
             // display the correct info based on their number
             firstTestimonial.classList.add('hidden');
             secondTestimonial.classList.add('hidden');
@@ -150,6 +155,7 @@ function handleTouchMove(e) {
             // "unselect" other two orange dots
             dotsContainer.children[0].classList.remove('selected');
             dotsContainer.children[1].classList.remove('selected');
+            console.log('Swipe left');
         }
 
         allolwSwipe = false;     
@@ -207,4 +213,56 @@ function handleTouchMove(e) {
    
         allolwSwipe = false;       
     }     
+}
+
+
+
+// ############################ Handle the hamburger menu
+const hamburgerMenuIcon = document.querySelector('.hamburger-menu-icon');
+
+hamburgerMenuIcon.addEventListener('click', openMenu);
+
+function openMenu() {
+    // change the image from hamburger to X
+    hamburgerMenuIcon.src = "/images/icon-close.svg";
+
+    let htmlMenu = document.createElement('div');
+    htmlMenu.classList.add('hamburger-menu-mobile');
+
+    htmlMenu.innerHTML = `
+        <ul>
+            <li class="list-item">Pricing</li>
+            <li class="list-item">Product</li>
+            <li class="list-item">About Us</li>
+            <li class="list-item">Careers</li>
+            <li class="list-item">Comunity</li>
+        </ul>
+    `;
+
+    // Select the first section
+    const firstSection = document.querySelector('.first-section');
+
+    // Select the parent element of first section and inside it, right before first section add "htmlMenu"
+    firstSection.parentElement.insertBefore(htmlMenu, firstSection);
+
+
+    // add an overlay over tha page so the menu is the main element that user sees
+    document.querySelector('.container').classList.add('overlay');
+}
+
+// close the hamburger menu by clicking on the overlay
+const container = document.querySelector('.container');
+container.addEventListener('click', closeMenu);
+
+function closeMenu(e) {
+    if (e.target.classList.contains('overlay')) {
+        // change the X (close) icon to back to hamburger icon
+        hamburgerMenuIcon.src = "/images/icon-hamburger.svg";
+
+        // remove the menu
+        document.querySelector('.hamburger-menu-mobile').remove();
+
+        // remove the overlay
+        container.classList.remove('overlay');
+    }
 }
